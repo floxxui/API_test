004c104e287f2f8d894381e1beb1c4a0cda93586
floxxui <60339661+floxxui@users.noreply.github.com>
2021-02-23 13:30:12 +0100

    Lite saker

---
+++ Api/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+            RestRequest request = new RestRequest("pokemon/blaziken");
+            IRestResponse response = client.Get(request);
+            Console.WriteLine(response.Content);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Pokemon.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace API_test
+{
+    public class Pokemon
+    {
+        
+    }
+}
